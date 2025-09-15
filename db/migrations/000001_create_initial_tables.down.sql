fa8f5cd301d28b1e7c7d6a384125183631faf465
ten10ten10 <wdyhap@gmail.com>
2025-09-16 01:49:44 +0900

    migration file

---
+++ db/migrations/000001_create_initial_tables.down.sql
@@ -0,0 +1,7 @@
+-- 000001_create_initial_tables.down.sql
+
+DROP TABLE IF EXISTS "ShareToken";
+DROP TABLE IF EXISTS "Member";
+DROP TABLE IF EXISTS "Schedule";
+DROP TABLE IF EXISTS "Trip";
+DROP TABLE IF EXISTS "User";
