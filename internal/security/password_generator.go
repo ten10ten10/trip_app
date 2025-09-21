29e4059a5756ad2294e95461009fbca1b054211d
ten10ten10 <wdyhap@gmail.com>
2025-09-22 04:06:58 +0900

    usecase: user (signup パスワード生成機能の実装)

---
+++ internal/security/password_generator.go
@@ -0,0 +1,34 @@
+package security
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type PasswordGenerator interface {
+	GeneratePassword() (string, string, error)
+}
+
+type passwordGenerator struct{}
+
+func NewPasswordGenerator() PasswordGenerator {
+	return &passwordGenerator{}
+}
+
+func (r *passwordGenerator) GeneratePassword() (string, string, error) {
+	bytes := make([]byte, 12)
+	if _, err := rand.Read(bytes); err != nil {
+		return "", "", err
+	}
+	initialPassword := base64.URLEncoding.EncodeToString(bytes)
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(initialPassword), bcrypt.DefaultCost)
+	if err != nil {
+		return "", "", err
+	}
+	hashPassword := string(hash)
+
+	return initialPassword, hashPassword, nil
+}
