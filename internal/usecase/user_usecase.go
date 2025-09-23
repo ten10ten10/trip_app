ce4e0cdcae6014f580c4a228b1461e292e953e16
ten10ten10 <wdyhap@gmail.com>
2025-09-23 17:31:57 +0900

    usecase: user (signup IsActiveがfalseのuserのsignup処理の分岐)

--- internal/usecase/user_usecase.go
+++ internal/usecase/user_usecase.go
@@ -42,49 +42,85 @@ func (uu *userUsecase) SignUp(ctx context.Context, name, email string) (*domain.
 	}
 
 	// check if email already exists
-	_, err := uu.ur.FindByEmail(ctx, email)
+	foundUser, err := uu.ur.FindByEmail(ctx, email)
+
+	// if the user with the email exists but is not active, update the user
 	if err == nil {
-		// find no error means email already exists
-		return nil, errors.New("email already exists")
-	}
-	if !errors.Is(err, gorm.ErrRecordNotFound) {
-		// some other error occurred
-		return nil, err
+		if foundUser.IsActive {
+			// find no error means email already exists
+			return nil, errors.New("email already exists")
+		}
+		// initPassword & hashPassword
+		rawPassword, hashPassword, err := uu.up.GeneratePassword()
+		if err != nil {
+			return nil, err
+		}
+		// generate verification token
+		rawToken, hashToken, err := uu.us.GenerateToken()
+		if err != nil {
+			return nil, err
+		}
+		expiresAt := time.Now().Add(30 * time.Minute)
+
+		// update foundUser's old data
+		foundUser.Name = name
+		foundUser.PasswordHash = string(hashPassword)
+		foundUser.VerificationTokenHash = &hashToken
+		foundUser.VerificationTokenExpiresAt = &expiresAt
+
+		// update DB
+		if err := uu.ur.Update(ctx, foundUser); err != nil {
+			return nil, err
+		}
+
+		// send verification email
+		if err := uu.ue.SendVerificationEmail(ctx, foundUser.Email, rawToken, rawPassword); err != nil {
+			return nil, err
+		}
+
+		return foundUser, nil
 	}
 
-	// initPassword & hashPassword
-	rawPassword, hashPassword, err := uu.up.GeneratePassword()
-	if err != nil {
-		return nil, err
+	// create new user, if the user with the email does not exist
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		// initPassword & hashPassword
+		rawPassword, hashPassword, err := uu.up.GeneratePassword()
+		if err != nil {
+			return nil, err
+		}
+
+		// generate verification token
+		rawToken, hashToken, err := uu.us.GenerateToken()
+		if err != nil {
+			return nil, err
+		}
+		expiresAt := time.Now().Add(30 * time.Minute)
+
+		// create user
+		user := &domain.User{
+			ID:                         uuid.New(),
+			Name:                       name,
+			Email:                      email,
+			PasswordHash:               string(hashPassword),
+			IsActive:                   false,
+			VerificationTokenHash:      &hashToken,
+			VerificationTokenExpiresAt: &expiresAt,
+			CreatedAt:                  time.Now(),
+			UpdatedAt:                  time.Now(),
+		}
+
+		if err := uu.ur.Create(ctx, user); err != nil {
+			return nil, err
+		}
+
+		// send verification email
+		if err := uu.ue.SendVerificationEmail(ctx, user.Email, rawToken, rawPassword); err != nil {
+			return nil, err
+		}
+
+		return user, nil
 	}
 
-	// generate verification token
-	rawToken, hashToken, err := uu.us.GenerateToken()
-	if err != nil {
-		return nil, err
-	}
-	expiresAt := time.Now().Add(30 * time.Minute)
-
-	// create user
-	user := &domain.User{
-		ID:                         uuid.New(),
-		Name:                       name,
-		Email:                      email,
-		PasswordHash:               string(hashPassword),
-		IsActive:                   false,
-		VerificationTokenHash:      &hashToken,
-		VerificationTokenExpiresAt: &expiresAt,
-		CreatedAt:                  time.Now(),
-		UpdatedAt:                  time.Now(),
-	}
-	if err := uu.ur.Create(ctx, user); err != nil {
-		return nil, err
-	}
-
-	// send verification email
-	if err := uu.ue.SendVerificationEmail(ctx, user.Email, rawToken, rawPassword); err != nil {
-		return nil, err
-	}
-
-	return user, nil
+	// other errors
+	return nil, err
 }
