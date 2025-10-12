500e431d0bd93d3138203adf55f3190712b228d1
ten10ten10 <wdyhap@gmail.com>
2025-10-12 18:37:55 +0900

    handler: all

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -9,11 +9,18 @@ import (
 type Handler struct {
 	*userHandler
 	*tripHandler
+	*scheduleHandler
 }
 
-func NewHandler(userUsecase usecase.UserUsecase, tripUsecase usecase.TripUsecase) api.ServerInterface {
+func NewHandler(
+	userUsecase usecase.UserUsecase,
+	tripUsecase usecase.TripUsecase,
+	scheduleUsecase usecase.ScheduleUsecase,
+	scheduleHandlerValidator ScheduleHandlerValidator,
+) api.ServerInterface {
 	return &Handler{
-		userHandler: NewUserHandler(userUsecase),
-		tripHandler: NewTripHandler(tripUsecase),
+		userHandler:     NewUserHandler(userUsecase),
+		tripHandler:     NewTripHandler(tripUsecase),
+		scheduleHandler: NewScheduleHandler(scheduleUsecase, scheduleHandlerValidator),
 	}
 }
