d9a7a02ce48260dfd77a080988b4b0f6f616c265
ten10ten10 <wdyhap@gmail.com>
2025-10-20 00:49:18 +0900

    refactor: column name

--- internal/domain/member.go
+++ internal/domain/member.go
@@ -3,7 +3,7 @@ package domain
 import "github.com/google/uuid"
 
 type Member struct {
-	ID     uuid.UUID `gorm:"type:uuid;default:uuid_generate_v7();primaryKey"`
-	TripID uuid.UUID `gorm:"type:uuid;not null;index"`
-	Name   string    `gorm:"size:255;not null"`
+	ID     uuid.UUID `gorm:"column:id;type:uuid;default:uuid_generate_v7();primaryKey"`
+	TripID uuid.UUID `gorm:"column:trip_id;type:uuid;not null;index"`
+	Name   string    `gorm:"column:name;size:255;not null"`
 }
