fccd63e369afaaf74d0326c37fb7d590f5b44d1b
ten10ten10 <wdyhap@gmail.com>
2025-10-20 00:49:22 +0900

    refactor: column name

--- internal/domain/schedule.go
+++ internal/domain/schedule.go
@@ -7,12 +7,12 @@ import (
 )
 
 type Schedule struct {
-	ID            uuid.UUID `gorm:"type:uuid;default:uuid_generate_v7();primaryKey"`
-	TripID        uuid.UUID `gorm:"type:uuid;not null;index"`
-	Title         string    `gorm:"size:255;not null"`
-	StartDateTime time.Time `gorm:"type:timestamptz;not null"`
-	EndDateTime   time.Time `gorm:"type:timestamptz;not null"`
-	Memo          string    `gorm:"type:text"`
-	CreatedAt     time.Time `gorm:"column:createdAt;type:timestamptz;not null;autoCreateTime:false"`
-	UpdatedAt     time.Time `gorm:"column:updatedAt;type:timestamptz;not null;autoUpdateTime:false"`
+	ID            uuid.UUID `gorm:"column:id;type:uuid;default:uuid_generate_v7();primaryKey"`
+	TripID        uuid.UUID `gorm:"column:trip_id;type:uuid;not null;index"`
+	Title         string    `gorm:"column:title;size:255;not null"`
+	StartDateTime time.Time `gorm:"column:start_date_time;type:timestamptz;not null"`
+	EndDateTime   time.Time `gorm:"column:end_date_time;type:timestamptz;not null"`
+	Memo          string    `gorm:"column:memo;type:text"`
+	CreatedAt     time.Time `gorm:"column:created_at;type:timestamptz;not null;autoCreateTime:false"`
+	UpdatedAt     time.Time `gorm:"column:updated_at;type:timestamptz;not null;autoUpdateTime:false"`
 }
