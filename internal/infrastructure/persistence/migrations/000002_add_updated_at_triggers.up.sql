26f59c77c4520bb423372ac756bafa9707f7f8b9
ten10ten10 <wdyhap@gmail.com>
2025-10-20 00:49:14 +0900

    refactor: column name

--- internal/infrastructure/persistence/migrations/000002_add_updated_at_triggers.up.sql
+++ internal/infrastructure/persistence/migrations/000002_add_updated_at_triggers.up.sql
@@ -2,7 +2,7 @@
 CREATE OR REPLACE FUNCTION update_updated_at_column()
 RETURNS TRIGGER AS $$
 BEGIN
-    NEW."updatedAt" = now();
+    NEW."updated_at" = now();
     RETURN NEW;
 END;
 $$ language 'plpgsql';
