59b146db6234115a50a8df78a902efd7e62e3aa2
ten10ten10 <wdyhap@gmail.com>
2025-10-20 00:49:32 +0900

    refactor: column name

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -9,14 +9,14 @@ import (
 )
 
 type User struct {
-	ID                         uuid.UUID `gorm:"type:uuid;default:uuid_generate_v7();primaryKey"`
-	Name                       string    `gorm:"size:255;not null"`
-	Email                      string    `gorm:"size:255;uniqueIndex;not null"`
-	PasswordHash               string    `gorm:"size:255;not null"`
-	IsActive                   bool      `gorm:"not null;default:false"`
-	VerificationTokenHash      *string   `gorm:"size:255;uniqueIndex"`
-	VerificationTokenExpiresAt *time.Time
-	CreatedAt                  time.Time `gorm:"column:createdAt;type:timestamptz;not null;autoCreateTime:false"`
-	UpdatedAt                  time.Time `gorm:"column:updatedAt;type:timestamptz;not null;autoUpdateTime:false"`
-	Trips                      []Trip    `gorm:"constraint:OnDelete:CASCADE"`
+	ID                         uuid.UUID `gorm:"column:id;type:uuid;default:uuid_generate_v7();primaryKey"`
+	Name                       string    `gorm:"column:name;size:255;not null"`
+	Email                      string    `gorm:"column:email;size:255;uniqueIndex;not null"`
+	PasswordHash               string    `gorm:"column:password_hash;size:255;not null"`
+	IsActive                   bool      `gorm:"column:is_active;not null;default:false"`
+	VerificationTokenHash      *string   `gorm:"column:verification_token_hash;size:255;uniqueIndex"`
+	VerificationTokenExpiresAt *time.Time `gorm:"column:verification_token_expires_at"`
+	CreatedAt                  time.Time `gorm:"column:created_at;type:timestamptz;not null;autoCreateTime:false"`
+	UpdatedAt                  time.Time `gorm:"column:updated_at;type:timestamptz;not null;autoUpdateTime:false"`
+	Trips                      []Trip    `gorm:"foreignKey:user_id;constraint:OnDelete:CASCADE"`
 }
