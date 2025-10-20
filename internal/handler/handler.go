73c552693ae0f91bec0eeb524070ee87e79ead81
ten10ten10 <wdyhap@gmail.com>
2025-10-21 02:05:24 +0900

    handler: all

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -10,18 +10,21 @@ type Handler struct {
 	*userHandler
 	*tripHandler
 	*scheduleHandler
+	*shareTokenHandler
 }
 
 func NewHandler(
 	userUsecase usecase.UserUsecase,
 	tripUsecase usecase.TripUsecase,
 	scheduleUsecase usecase.ScheduleUsecase,
+	shareTokenUsecase usecase.ShareTokenUsecase,
 	userHandlerValidator UserHandlerValidator,
 	scheduleHandlerValidator ScheduleHandlerValidator,
 ) api.ServerInterface {
 	return &Handler{
-		userHandler:     NewUserHandler(userUsecase, userHandlerValidator),
-		tripHandler:     NewTripHandler(tripUsecase),
-		scheduleHandler: NewScheduleHandler(scheduleUsecase, scheduleHandlerValidator),
+		userHandler:       NewUserHandler(userUsecase, userHandlerValidator),
+		tripHandler:       NewTripHandler(tripUsecase),
+		scheduleHandler:   NewScheduleHandler(scheduleUsecase, scheduleHandlerValidator),
+		shareTokenHandler: NewShareTokenHandler(shareTokenUsecase),
 	}
 }
